4d34567657d005b119a0bcb1e89d04fe74866698
Amirktb1994 <amir.kotobi@desy.de>
2023-08-25 16:02:55 +0200

    solving import problem in XASNet

--- src/XASNet/models.py
+++ src/XASNet/models.py
@@ -11,8 +11,8 @@ from torch_geometric.nn import global_add_pool
 
 from .utils.weight_init import kaiming_orthogonal_init
 
-from XASNet_GAT.base_layers import *
-from XASNet_GraphNet import GraphNetwork
+from .XASNet_GAT.base_layers import *
+from .XASNet_GraphNet.modules import GraphNetwork
 
 gnn_layers = {
     'gat': geomnn.GATConv,
